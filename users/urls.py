b98e1d4e30bb2dfafb461d29cfb817f33fa17245
Mehedi-Hasan-18 <mdmehedihasanroby@gmail.com>
2025-05-14 01:24:53 +0800

    Assignment 3 Done

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from users.views import register,signIn,active,create_group,signOut
+from users.views import CreateGroupView,LogoutView,RegisterView,CustomLoginView,AccountActivationView,ProfileView,ChangePassword,PasswordChangeDoneView,CustomPasswordReseConfirmtView,CustomPasswordResetView,CustomLogoutView
 
 urlpatterns = [
-    path('register/', register, name="register"),
-    path('sign-in/', signIn, name="signIn"),
-    path('sign-out/', signOut, name="signOut"),
-    path('activate/<int:user_id>/<str:token>',active, name='active'),
-    path('admin/create-group',create_group, name='create-group'),
+    # path('register/', register, name="register"),
+    path('register/', RegisterView.as_view(), name="register"),
+    path('sign-in/', CustomLoginView.as_view(), name="signIn"),
+    path('sign-out/', CustomLogoutView.as_view(), name="signOut"),
+    path('activate/<int:user_id>/<str:token>',AccountActivationView.as_view(), name='active'),
+    path('admin/create-group',CreateGroupView.as_view(), name='create-group'),
+    path('profile/',ProfileView.as_view(), name='profile' ),
+    path('change-password/', ChangePassword.as_view(), name='change_password' ),
+    path('change-password/done/', PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html'), name='password_change_done/' ),
+    path('password-reset/', CustomPasswordResetView.as_view(), name='password_reset' ),
+    path('password-reset/confirm/<uidb64>/<token>', CustomPasswordReseConfirmtView.as_view(), name='password_reset_confirm' ),
 ]
