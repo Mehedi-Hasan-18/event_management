4b491951629ab179c3ed441207839356e6d8b82c
Mehedi-Hasan-18 <mdmehedihasanroby@gmail.com>
2025-07-28 15:58:04 +0800

    hide toolbar

--- eventmanagement/urls.py
+++ eventmanagement/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
 from core.views import home
-from debug_toolbar.toolbar import debug_toolbar_urls
+# from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -10,7 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('events/',include('events.urls')),
     path('users/',include('users.urls')),
-]+ debug_toolbar_urls()
+]
 
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
